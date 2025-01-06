84960c080dd6c799a961e8e61c2016cfee372d06
peter88213 <petert88213@gmail.com>
2025-01-06 23:08:35 +0100

    Update docstrings.

--- src/nv_zim.py
+++ src/nv_zim.py
@@ -1,7 +1,7 @@
 """Zim Desktop Wiki connection plugin for novelibre.
 
 Requires Python 3.6+
-Copyright (c) 2024 Peter Triesberger
+Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_zim
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 
--- src/nvzim/wiki_manager.py
+++ src/nvzim/wiki_manager.py
@@ -1,6 +1,6 @@
 """Provide a service class for a Zim Desktop Wiki connection manager.
 
-Copyright (c) 2024 Peter Triesberger
+Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_zim
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
