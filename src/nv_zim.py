036e0d24252744967abcc114b151c82615ab1735
peter88213 <petert88213@gmail.com>
2025-09-04 12:59:03 +0200

    Updated docstring: Python 3.7+ required.

--- src/nv_zim.py
+++ src/nv_zim.py
@@ -1,6 +1,6 @@
 """Zim Desktop Wiki connection plugin for novelibre.
 
-Requires Python 3.6+
+Requires Python 3.7+
 Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_zim
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
